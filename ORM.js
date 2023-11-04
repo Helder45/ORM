396e46a73002cb7c046e4ae8aab097a8692f738b
Helder45 <helder.pereira@estudante.ifms.edu.br>
2023-11-04 16:25:24 -0400

    git init repository

---
+++ ORM.js
@@ -0,0 +1,42 @@
+class ORM {
+    constructor() {
+        throw new Error("Classe ORM não pode ser instanciada diretamente!");
+    }
+
+    menu() {
+
+        do{
+            console.log("");
+        }
+        
+    }
+
+    static criar(){
+
+    }
+
+    static atualizar() {
+
+    }
+
+    static remover() {
+
+    }
+
+    static buscar() {
+
+    }
+
+}
+
+class Pessoa extends ORM{
+    constructor() {
+        
+    }
+}
+
+class Animal extends ORM{
+    constructor() {
+        
+    }
+}
